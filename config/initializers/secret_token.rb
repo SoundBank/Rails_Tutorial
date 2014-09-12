f44d82873b74a5d540f7559f45ce8a4e893bb79c
SoundBank <cat.urandom@gmail.com>
2014-09-12 01:05:17 +0100

    Initial commit, baby!

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SampleApp::Application.config.secret_key_base = '607a8f60f69d460566f3a3e3782fa05c812fe34ad85b5165db7517fcd0b325dd90e39e9407da3417edc6bcb77efdd27377ffc0099dc0e68438891e0906543795'
