37f81dedad121e6d528b1a6c1c1c08a50cf88baf
Peet <peet.hudson@yandex.com>
2014-09-15 07:50:56 +0100

    9.1.2 begins. - App working but debugging broken.

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -35,27 +35,26 @@ subject { page }
 			describe "after saving the user" do
 			before { click_button submit }
 			let(:user) { User.find_by(email: 'user@example.com') }
-			
-				describe "edit" do
-				let(:user) { FactoryGirl.create(:user) }
-				before { visit edit_user_path(user) }
-
-					describe "page" do
-					it { should have_content("Update your profile") }
-					it { should have_title("Edit user") }
-					it { should have_link('change', href: 'http://gravatar.com/emails') }
-					end
-
-				describe "with invalid information" do
-				before { click_button "Save changes" }
-				it { should have_content('error') }
-				end
-			end
-			
+						
 			it { should have_link('Sign out') }
 			it { should have_title(user.name) }
 			it { should have_selector('div.alert.alert-success', text: 'Welcome') }
 			end				
 		end
 	end
+#				describe "edit" do
+#				let(:user) { FactoryGirl.create(:user) }
+#				before { visit edit_user_path(user) }
+#
+#					describe "page" do
+#					it { should have_content("Update your profile") }
+#					it { should have_title("Edit user") }
+#					it { should have_link('change', href: 'http://gravatar.com/emails') }
+#					end
+#
+#				describe "with invalid information" do
+#				before { click_button "Save changes" }
+#				it { should have_content('error') }
+#				end
+#			end	
 end
