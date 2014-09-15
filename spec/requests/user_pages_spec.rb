07cc8381cfa38ab16746e2cb85061a2b7130c24a
Peet <peet.hudson@yandex.com>
2014-09-15 05:17:44 +0100

    9.2.2 begins - All 52 Green.

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -13,6 +13,7 @@ subject { page }
 	describe "signup page" do
 	before { visit signup_path }
 	let(:submit) { "Create my account" }
+	
 		describe "with invalid information" do
 			it "should not create a user" do
 			expect { click_button submit }.not_to change(User, :count)
@@ -34,27 +35,27 @@ subject { page }
 			describe "after saving the user" do
 			before { click_button submit }
 			let(:user) { User.find_by(email: 'user@example.com') }
+			
+				describe "edit" do
+				let(:user) { FactoryGirl.create(:user) }
+				before { visit edit_user_path(user) }
 
+					describe "page" do
+					it { should have_content("Update your profile") }
+					it { should have_title("Edit user") }
+					it { should have_link('change', href: 'http://gravatar.com/emails') }
+					end
+
+				describe "with invalid information" do
+				before { click_button "Save changes" }
+				it { should have_content('error') }
+				end
+			end
+			
 			it { should have_link('Sign out') }
 			it { should have_title(user.name) }
 			it { should have_selector('div.alert.alert-success', text: 'Welcome') }
 			end				
 		end
 	end
-	
-	describe "edit" do
-	let(:user) { FactoryGirl.create(:user) }
-	before { visit edit_user_path(user) }
-
-		describe "page" do
-		it { should have_content("Update your profile") }
-		it { should have_title("Edit user") }
-		it { should have_link('change', href: 'http://gravatar.com/emails') }
-		end
-
-		describe "with invalid information" do
-		before { click_button "Save changes" }
-		it { should have_content('error') }
-		end
-	end	
 end
