c097702311520fa797dd4547faa8328ba8e68b8a
Peet <peet.hudson@yandex.com>
2014-09-17 17:43:17 +0100

    10.2.2 begins - All Green - 89

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,9 +7,10 @@ before_action :admin_user,     only: :destroy
 		@users = User.paginate(page: params[:page])
 	end 
  
-	def show
-		@user = User.find(params[:id])
-	end
+  def show
+    @user = User.find(params[:id])
+    @microposts = @user.microposts.paginate(page: params[:page])
+  end
 
 	def new
 		@user = User.new  
