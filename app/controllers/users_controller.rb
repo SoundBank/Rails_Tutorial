5378c54a7d3a33cac55f8c6329569b216c0db477
Peet <peet.hudson@yandex.com>
2014-09-17 22:52:38 +0100

    10.3.3 begins - Rspec abandoned before micropost tests

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,4 @@
 class UsersController < ApplicationController
-before_action :signed_in_user, only: [:index, :edit, :update, :destroy]
 before_action :correct_user,   only: [:edit, :update]
 before_action :admin_user,     only: :destroy 
  
@@ -56,10 +55,6 @@ private
 
     # Before filters
 
-    def signed_in_user
-		redirect_to signin_url, notice: "Please sign in." unless signed_in?
-    end
-	
 	def correct_user
 		@user = User.find(params[:id])
 		redirect_to(root_url) unless current_user?(@user)
